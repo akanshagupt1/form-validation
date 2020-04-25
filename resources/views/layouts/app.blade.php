14400e51a299b99e29ef59f436c7046a32533fa0
akanshagpt1 <gakansha192@gmail.com>
2020-04-25 22:40:29 +0530

    project setup

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+    @yield('footer')
+</body>
+
+</html>
